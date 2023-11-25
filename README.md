ef365eed45de256e4a064f669b4d0f6f6eed0ac3
JMCA2805 <josemauricio2805@gmail.com>
2023-11-24 22:16:24 -0400

    Creando la estructura base del BackEnd

---
+++ README.md
@@ -0,0 +1,6 @@
+# **Autores:**
+
+- José Escalona / C.I: 28.206.133
+- Maikel Villegas Rojas / C.I: 30.302.836
+- Luis Monsalve / C.I: 30.380.310
+- José Camacho / C.I: 29.739.132
---
+++ src/app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+const cron = require('node-cron');
+const {enviarOferta} = require("./scripts/nodemailer")
+
+///INICIALIZACIONES
+var app = express();
+
+// MIDDLEWARES
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+cron.schedule('0 8 * * *', () => {
+  enviarOferta()
+}) 
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// ROUTES
+const indexRuta = require("./routes/index");
+
+app.use("/", indexRuta);
+
+module.exports = app;
