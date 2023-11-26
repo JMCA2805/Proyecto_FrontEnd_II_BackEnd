9bde29e7598dcf555773f89b0bfbd099359932dd
yetzeniam7 <mendozaaya@uvm.edu.ve>
2023-11-26 18:51:36 -0400

    Agregando los controllers de los productos
    Close #9

---
+++ src/controllers/productos.js
@@ -0,0 +1,249 @@
+const { json } = require('body-parser');
+const products = require('../models/products');
+const Usuario = require("../models/user.js");
+
+const sharp = require('sharp');
+const { Binary } = require('mongodb');
+
+class productosController {
+  obtenerProductos = async (req, res) => {
+    console.log(req.query)
+    try {
+      const productoscom = await products.find();
+      let imagenCompleta
+      let data
+      let productos = []
+
+      for(let i = 0; i<productoscom.length;i++){
+
+       
+        productos[i] = {
+          serial: productoscom[i].serial,
+          nombre: productoscom[i].nombre,
+          descripcion: productoscom[i].descripcion,
+          cantidad: productoscom[i].cantidad,
+          precio: productoscom[i].precio,
+          categoria: productoscom[i].categoria
+  
+        }
+      }
+
+      if (productos.length === 0) {
+        res.status(200).send('No hay productos en la Base de Datos');
+      } else {
+        res.status(200).json(productos);
+      }
+    } catch (error) {
+      res.status(500).json({ Error: 'Error al obtener productos' });
+    }
+  };
+
+  agregarProductos = async (req, res) => {
+    try {
+      if (req.body.nombreBoton == 'Buscar'){
+        let productos = []
+        let data
+        let imagenCompleta
+
+        if(req.body.filtro == "Nombre"){
+
+          const nombre = req.body.busqueda
+          if (!RegExp.escape) {
+            RegExp.escape = function(s) {
+              return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+            };
+          }
+    
+          var regex = new RegExp('.*' + RegExp.escape(nombre) + '.*', 'i');
+          const productoscom = await products.find({nombre: regex});
+    
+    
+          for(let i = 0; i<productoscom.length;i++){
+    
+            data = productoscom[i].imagen.data
+            imagenCompleta = 'data:'+productoscom[i].imagen.contentType+";base64,"+data.toString('base64')
+    
+            productos[i] = {
+              serial: productoscom[i].serial,
+              nombre: productoscom[i].nombre,
+              descripcion: productoscom[i].descripcion,
+              cantidad: productoscom[i].cantidad,
+              precio: productoscom[i].precio,
+              categoria: productoscom[i].categoria//,
+            //   imagen: imagenCompleta,
+      
+            }
+          }
+    
+          res.status(200).json(productos);
+
+        }else if(req.body.filtro == "Categoria"){
+
+          const categoria = req.body.categoria
+
+          const productoscom = await products.find({categoria: categoria});
+
+          for(let i = 0; i<productoscom.length;i++){
+    
+            data = productoscom[i].imagen.data
+            imagenCompleta = 'data:'+productoscom[i].imagen.contentType+";base64,"+data.toString('base64')
+    
+            productos[i] = {
+              serial: productoscom[i].serial,
+              nombre: productoscom[i].nombre,
+              descripcion: productoscom[i].descripcion,
+              cantidad: productoscom[i].cantidad,
+              precio: productoscom[i].precio,
+              categoria: productoscom[i].categoria,
+              imagen: imagenCompleta,
+      
+            }
+          }
+
+          res.status(200).json(productos);
+        }
+
+
+      }else if((req.body.nombreBoton == 'Favorito')){
+
+        const { idUser, serial, action } = req.body;
+
+        if (action === 'Agregar') {
+          try {
+            // Busca al usuario por su idUser
+            const usuario = await Usuario.findOne({ _id: idUser });
+      
+            if (usuario) {
+              // Agrega el serial al arreglo de favoritos
+
+              const existeSerial = usuario.favoritos.includes(serial);
+              
+              if (existeSerial) {
+                return
+              }
+
+              usuario.favoritos.push(serial);
+      
+              // Guarda los cambios en el usuario
+              await usuario.save();
+            } 
+          } catch (error) {
+            return res.status(500).json({
+              message: 'Error al agregar el producto a favoritos',
+              error: error.message
+            });
+          }
+        } else if (action === 'Quitar') {
+          try {
+            // Busca al usuario por su idUser
+            const usuario = await Usuario.findOne({ _id: idUser });
+      
+            if (usuario) {
+              // Verifica si el producto está en la lista de favoritos
+              const index = usuario.favoritos.indexOf(serial);
+              if (index > -1) {
+                // Elimina el producto de la lista de favoritos
+                usuario.favoritos.splice(index, 1);
+      
+                // Guarda los cambios en el usuario
+                await usuario.save();
+      
+              }
+            }
+          } catch (error) {
+            return res.status(500).json({
+              message: 'Error al eliminar el producto de favoritos',
+              error: error.message
+            });
+          }
+        }
+
+      }else{
+        const { serial, nombre, descripcion, cantidad, precio, categoria } = req.body;
+        const imagenBuffer = req.file.buffer;
+        const contentType = req.file.mimetype;
+        
+        const serialEquipos = await products.findOne({ serial });
+  
+        if (serialEquipos) {
+          res.status(400).send('Serial de equipo ya registrado');
+        } else {
+          const nuevoproducto = new products({
+            serial,
+            nombre,
+            descripcion,
+            cantidad,
+            precio,
+            categoria,
+            imagen: { data:imagenBuffer , contentType}
+  
+          });
+          await nuevoproducto.save();
+          res.status(201).send('Producto agregado correctamente');
+        }
+      }
+
+    } catch (error) {
+      console.error('Error al agregar producto:', error);
+      res.status(500).json({ Error: 'Error al agregar producto' });
+    }
+  };
+  
+  busquedaProductos = async (req, res) => {
+    try {
+
+        const serial = req.params.serial;
+        const productos = await products.findById(serial);
+
+
+        if (productos == ''){
+            res.status(200).send('No hay productos en la Base de Datos');
+        } else {
+            res.status(200).send(productos);
+        }
+
+    } catch (error) {
+      res.status(404).json({Error: 'Error al obtener productos'})
+    }
+  };
+
+  eliminarProducto = async (req, res) => {
+    try {
+      const serial = req.body.serial;
+
+      const producto = await products.findOne({ serial });
+      if (!producto) {
+        return res.status(404).json({ mensaje: 'Producto no encontrado' });
+      }
+
+      await products.deleteOne({'serial': serial});
+
+      res.json({ mensaje: 'Producto eliminado correctamente' });
+      
+    } catch (error) {
+      console.error('Error al eliminar el producto:', error);
+      res.status(500).json({ mensaje: 'Error al eliminar el producto' });
+    }
+  };
+
+
+  editarProducto = async (req, res) => {
+    try {
+      const { serial, datosActualizados } = req.body;
+      const producto = await products.findOne({ serial });
+      if (!producto) {
+        return res.status(404).json({ mensaje: 'Producto no encontrado' });
+      }
+
+      await products.findOneAndUpdate({ serial },datosActualizados,{ new: true })
+
+      res.json({ mensaje: 'Producto editado correctamente' });
+      
+    } catch (error) {
+      console.error('Error al editae el producto:', error);
+      res.status(500).json({ mensaje: 'Error al editar el producto' });
+    }
+  };
+}
+const productosC = new productosController();
+module.exports = productosC;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const controller = require('../controllers/productos');
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = path.join(__dirname, '../uploads');
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.get('/', (req, res) => {
+  controller.obtenerProductos(req, res);
+});
+
+router.post('/', upload.single('imagen'), (req, res) => {
+  controller.agregarProductos(req, res);
+});
+
+router.delete('/', (req, res) => {
+  controller.eliminarProducto(req, res);
+});
+
+router.put('/', (req, res) => {
+  controller.editarProducto(req, res);
+});
 
 module.exports = router;
