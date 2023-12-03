ea0c34cf9da8c89f12e887709b523a1c82bcfe09
yetzeniam7 <mendozaaya@uvm.edu.ve>
2023-12-03 16:33:56 -0400

    Agregando controlador de Favoritos

--- src/controllers/user-c.js
+++ src/controllers/user-c.js
@@ -16,6 +16,7 @@ class usuariosController {
           telefono: usuariosCompletos[i].telefono,
         };
 
+
         usuarios.push(usuario);
       }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const controller = require('../controllers/productos');
+const favorito = require('../controllers/favorito')
 
 const multer = require('multer');
 
@@ -30,5 +31,18 @@ router.delete('/productos', (req, res) => {
 router.put('/productos', upload.single('imagen'), (req, res) => {
   controller.editarProducto(req, res);
 });
+
+
+router.post('/favorito/', (req, res) => {
+  favorito.Add_Fav(req, res);
+});
+router.get('/favorito/', (req, res) => {
+  favorito.Fav_List(req, res);
+});
+
+router.delete('/favorito', (req, res) => {
+  favorito.Del_Fav(req, res);
+});
+
 
 module.exports = router;
